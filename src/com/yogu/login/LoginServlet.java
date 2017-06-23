309b20cd7e74ab725e96987bf40ea1aeed0e1d58
Yogeswari A <yogeshwari.arjunan@adaptavantcloud.com>
2017-06-23 22:09:01 +0530

    Implementing data store.

--- src/com/yogu/login/LoginServlet.java
+++ src/com/yogu/login/LoginServlet.java
@@ -1,20 +1,18 @@
 package com.yogu.login;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import com.full.model.UserDetails;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
 
 @SuppressWarnings("serial")
 public class LoginServlet extends HttpServlet {
-	private static List<UserDetails> users = new ArrayList<>();
+	DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 
 	@Override
 	public void init() {
@@ -48,23 +46,20 @@ public class LoginServlet extends HttpServlet {
 
 		if ((emailId != null && !emailId.isEmpty()) && (password != null && !password.isEmpty())) {
 			HttpSession session = req.getSession();
-			for (UserDetails user : users) {
-				if (user.getEmailId().equalsIgnoreCase(emailId)) {
-					if (password != null && password.equals(user.getPassword())) {
-
-						session.setAttribute("username", user.getFirstName() + " " + user.getLastName());
-						req.getRequestDispatcher("/main").forward(req, resp);
-					} else {
-						resp.getWriter().println("Username or Password is wrong");
-					}
-					break;
-				}
-			}
-
-			if (session.getAttribute("username") == null) {
-				resp.getWriter().println("Your not sigup.Please singup. ");
-			}
-		} else {
+			/*
+			 * for (UserDetails user : entity) { if
+			 * (user.getEmailId().equalsIgnoreCase(emailId)) { if (password !=
+			 * null && password.equals(user.getPassword())) {
+			 * 
+			 * session.setAttribute("username", user.getFirstName() + " " +
+			 * user.getLastName());
+			 * req.getRequestDispatcher("/main").forward(req, resp); } else {
+			 * resp.getWriter().println("Username or Password is wrong"); }
+			 * break; } }
+			 * 
+			 * if (session.getAttribute("username") == null) {
+			 * resp.getWriter().println("Your not sigup.Please singup. "); }
+			 */ } else {
 			resp.getWriter().println("Please give correct credentials");
 		}
 
@@ -73,59 +68,61 @@ public class LoginServlet extends HttpServlet {
 	private void signUpUser(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String firstName = req.getParameter("firstName");
 		String lastName = req.getParameter("lastName");
-
 		String emailId = req.getParameter("emailId");
-
 		String password = req.getParameter("password");
 		String gender = req.getParameter("gender");
 		String dateOfBirth = req.getParameter("birthDay");
 
-		UserDetails user = new UserDetails();
-		if (firstName != null && !firstName.isEmpty()) {
-			user.setFirstName(firstName);
-		} else {
-			resp.getWriter().println("First name is mandatory.");
-		}
-
-		if (lastName != null && !lastName.isEmpty()) {
-			user.setLastName(lastName);
-		} else {
-			resp.getWriter().println("Last name is mandatory.");
-		}
-
-		if ((emailId != null && !emailId.isEmpty())) {
-			user.setEmailId(emailId);
-		} else {
-			resp.getWriter().println("Email id is mandatory.");
-		}
-
-		if (password != null && !password.isEmpty()) {
-			user.setPassword(password);
-		} else {
-			resp.getWriter().println("password is mandatory.");
-		}
-
-		user.setGender(gender);
-		user.setDateOfBirth(dateOfBirth);
-		boolean isUserExists = false;
-		for (UserDetails userDetail : users) {
-			if (userDetail.getEmailId().equals(emailId)) {
-				resp.getWriter().println("Email id is already exists ");
-				isUserExists = true;
-				break;
-			}
-		}
-
-		if (!isUserExists && (firstName != null && !firstName.isEmpty()) && (lastName != null && !lastName.isEmpty())
+		if ((firstName != null && !firstName.isEmpty()) && (lastName != null && !lastName.isEmpty())
 				&& (emailId != null && !emailId.isEmpty()) && (password != null && !password.isEmpty())) {
+			Entity entity = new Entity("Users", emailId);
+			entity.setProperty("FirstName", firstName);
+			entity.setProperty("LastName", lastName);
+			entity.setProperty("EmailId", emailId);
+			entity.setProperty("Password", password);
+			entity.setProperty("Gender", gender);
+			entity.setProperty("DateOfBirth", dateOfBirth);
 
-			users.add(user);
+			datastore.put(entity);
 			HttpSession session = req.getSession();
 			session.setAttribute("username", firstName + " " + lastName);
 			resp.getWriter().println("Successfully signed up.");
 			resp.sendRedirect("/main");
+		} else {
+			resp.getWriter().println("Please give correct credentials");
 		}
-	}
+		/*
+		 * if (firstName != null && !firstName.isEmpty()) {
+		 * user.setFirstName(firstName); } else {
+		 * resp.getWriter().println("First name is mandatory."); }
+		 * 
+		 * if (lastName != null && !lastName.isEmpty()) {
+		 * user.setLastName(lastName); } else {
+		 * resp.getWriter().println("Last name is mandatory."); }
+		 * 
+		 * if ((emailId != null && !emailId.isEmpty())) {
+		 * user.setEmailId(emailId); } else {
+		 * resp.getWriter().println("Email id is mandatory."); }
+		 * 
+		 * if (password != null && !password.isEmpty()) {
+		 * user.setPassword(password); } else {
+		 * resp.getWriter().println("password is mandatory."); }
+		 * 
+		 * user.setGender(gender); user.setDateOfBirth(dateOfBirth); boolean
+		 * isUserExists = false; for (UserDetails userDetail : users) { if
+		 * (userDetail.getEmailId().equals(emailId)) {
+		 * resp.getWriter().println("Email id is already exists "); isUserExists
+		 * = true; break; } }
+		 * 
+		 * if (!isUserExists && (firstName != null && !firstName.isEmpty()) &&
+		 * (lastName != null && !lastName.isEmpty()) && (emailId != null &&
+		 * !emailId.isEmpty()) && (password != null && !password.isEmpty())) {
+		 * 
+		 * users.add(user); HttpSession session = req.getSession();
+		 * session.setAttribute("username", firstName + " " + lastName);
+		 * resp.getWriter().println("Successfully signed up.");
+		 * resp.sendRedirect("/main"); }
+		 */ }
 
 	@Override
 	public void destroy() {
