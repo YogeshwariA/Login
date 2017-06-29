d8dfd4f1774b565357eb0aeb0b81356e0f03603f
Yogeswari A <yogeshwari.arjunan@adaptavantcloud.com>
2017-06-29 21:40:34 +0530

    added html folder html files,objectity library

---
+++ src/com/full/model/User.java
@@ -0,0 +1,62 @@
+package com.full.model;
+
+import java.util.Date;
+import com.googlecode.objectify.annotation.Entity;
+
+@Entity
+public class User {
+	private String firstName;
+	private String lastName;
+	private String emailId;
+	private String password;
+	private Date dateofBirth;
+	private String gender;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Date getDateofBirth() {
+		return dateofBirth;
+	}
+
+	public void setDateofBirth(Date dateofBirth) {
+		this.dateofBirth = dateofBirth;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+}
